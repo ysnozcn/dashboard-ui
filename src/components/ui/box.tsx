c4e74cf1872604f5cf3cbd2c2f41d73b555612d5
Yasino <yasino@outlook.com>
2025-02-26 19:59:36 +0300

    box ui style update

--- src/components/ui/box.tsx
+++ src/components/ui/box.tsx
@@ -13,13 +13,13 @@ const Box = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow-sm p-4",
+      "rounded-xl border border-primary/5 bg-card text-card-foreground p-4",
       className
     )}
     {...props}
   >
     {header && (
-      <div className="mb-4 pb-2 border-b">
+      <div className="mb-4 pb-2 border-b border-primary/5">
         <h3 className="text-lg font-semibold">{header}</h3>
       </div>
     )}
